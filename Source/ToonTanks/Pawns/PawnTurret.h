bdde8d052fcce5d99fe2f2471f9a21d84d84d8e3
jeevitha <jeevithasakthi137@gmail.com>
2020-06-29 21:24:41 +0530

    Create TankTurret Class

---
+++ Source/ToonTanks/Pawns/PawnTurret.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnBase.h"
+#include "PawnTurret.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API APawnTurret : public APawnBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float FireRate = 2.0f;
+
+    void CheckFireCondition();
+	FTimerHandle FireRateTimerHandle;
+
+protected:
+
+};
